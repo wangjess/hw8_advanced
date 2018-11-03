3e4a65ded0c59bfa6285f1495cfaa28bb8478354
Jessica Wang <wangjess@umich.edu>
2018-11-03 20:56:08 +0000

    lec files

---
+++ test_rpn.py
@@ -0,0 +1,15 @@
+import unittest
+import rpn
+
+class TestBasics(unittest.TestCase):
+    def test_add(self):
+        result = rpn.calculate("1 1 +")
+        self.assertEqual(2, result)
+
+    def test_subtract(self):
+        result = rpn.calculate("4 3 -")
+        self.assertEqual(1, result)
+
+    def test_toomany(self):
+        with self.assertRaises(ValueError):
+            result = rpn.calculate("1 2 3 +")
