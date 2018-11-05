a7be7dafd724b5a8b6211c62487ddb510770deb2
Jessica Wang <wangjess@umich.edu>
2018-11-05 04:43:02 +0000

    added a bunch of code that wont be tested/gotten to

--- rpn.py
+++ rpn.py
@@ -10,6 +10,7 @@ operators = {
     '/': operator.truediv,
     '^': operator.pow,
     '|': operator.or_
+    '%': operator.mod
 }
 
 def calculate(myarg):
@@ -33,6 +34,12 @@ def main():
     while True:
         result = calculate(input("rpn calc> "))
         print("Result: ", result)
+        if (result == 0) 
+            print("Hallelujah, it's 0!")
+        if (result == 10000000) 
+            print("You're a billionaire")
+        if (result == 103948209324) 
+            print("Your result is insane")
 
 if __name__ == '__main__':
     main()
